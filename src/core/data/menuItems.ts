a69e958f7116913b3c2301b66ad4a7daac3b2cc9
Alex Coronell <alexcoronell@gmail.com>
2024-09-30 20:48:42 -0500

    Learning component created

--- src/core/data/menuItems.ts
+++ src/core/data/menuItems.ts
@@ -3,6 +3,7 @@ import { type MenuItem } from "../models/MenuItem.interface";
 const menuItems: MenuItem[] = [
   { name: "About", url: "About" },
   { name: "Skills", url: "Skills" },
+  { name: "Learning", url: "Learning" },
   { name: "Works", url: "Works" },
   { name: "Experience", url: "Experiences" },
   { name: "Contact", url: "Contact" },
