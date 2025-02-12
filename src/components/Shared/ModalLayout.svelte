e4baddc0443a6e8061e429c5f39fd36f2aa6807e
Alex Coronell <alexcoronell@gmail.com>
2025-02-12 08:09:27 -0500

    ModalLayout partial completed

---
+++ src/components/Shared/ModalLayout.svelte
@@ -0,0 +1,15 @@
+<div class="ModalLayout">
+    <div class="ModalLayout__box">
+        <slot />
+    </div>
+</div>
+
+<style lang="scss">
+    .ModalLayout {
+        @apply fixed top-0 left-0 bottom-0 right-0 w-full h-screen bg-black/70 z-50 flex items-center justify-center;
+
+        &__box {
+            @apply bg-primary-dark border border-primary p-3;
+        }
+    }
+</style>
